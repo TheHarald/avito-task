51eb974ff8adfddf12f1e99acb4fa52ae61de4ce
Roman Surinov <romasurinov13@mail.ru>
2022-11-11 12:37:33 +0300

    change function folder

--- src/Functions/supportingFunctions.ts
+++ src/Functions/supportingFunctions.ts
@@ -1,4 +1,5 @@
 export function getTime(time: number | undefined): string {
+
     const date = new Date(Number(time) * 1000);
     return `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
 }

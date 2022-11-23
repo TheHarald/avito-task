256c90b981ad32668e2446dc004a95981604d01d
Roman Surinov <romasurinov13@mail.ru>
2022-11-24 01:46:49 +0300

    add task to readme

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -13,3 +13,5 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
+
+type Animal = 'cat' | 'dog' | 'pig'
